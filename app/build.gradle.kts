bc064054ca47bcaea4c92c59fb4f211f2dd14792
Dongmin <pdm001125@naver.com>
2024-03-11 11:41:36 +0900

    add/#1 : navigation mvi 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.bundles.orbit)
+    implementation(libs.bundles.navigation)
 }
