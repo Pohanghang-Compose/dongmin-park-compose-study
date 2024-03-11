67a4cc99016b4aafa8690a5099e5dd57a825681f
Dongmin <pdm001125@naver.com>
2024-03-11 14:50:45 +0900

    feat/#1: Sign Up UI 구현

--- app/src/main/java/com/chattymin/sopt_compose/feature/signup/SignUpPage.kt
+++ app/src/main/java/com/chattymin/sopt_compose/feature/signup/SignUpPage.kt
@@ -1,19 +1,88 @@
 package com.chattymin.sopt_compose.feature.signup
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.chattymin.sopt_compose.R
+import com.chattymin.sopt_compose.components.TitleText
+import com.chattymin.sopt_compose.ext.addFocusCleaner
+import com.chattymin.sopt_compose.feature.signin.TitleWithEtv
+import com.chattymin.sopt_compose.ui.theme.SoptcomposeTheme
 
 @Composable
 fun SignUpPage(navController: NavController) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "SignUp")
+    val focusManager = LocalFocusManager.current
+
+    val id = remember { mutableStateOf("") }
+    val pw = remember { mutableStateOf("") }
+    val nickname = remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TitleText(text = stringResource(id = R.string.sign_up_title))
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(20.dp)
+                .addFocusCleaner(focusManager),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            TitleWithEtv(
+                title = stringResource(id = R.string.id),
+                value = id,
+                hint = stringResource(id = R.string.auth_id_hint)
+            )
+
+            Spacer(modifier = Modifier.padding(vertical = 20.dp))
+
+            TitleWithEtv(
+                title = stringResource(id = R.string.pw),
+                value = pw,
+                hint = stringResource(id = R.string.auth_pw_hint)
+            )
+
+            Spacer(modifier = Modifier.padding(vertical = 20.dp))
+
+            TitleWithEtv(
+                title = stringResource(id = R.string.nicknmae),
+                value = nickname,
+                hint = stringResource(id = R.string.auth_nickname_hint)
+            )
+
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = stringResource(id = R.string.sign_up_btn))
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+@Preview
+fun SignUpPreview() {
+    SoptcomposeTheme {
+        SignUpPage(navController = rememberNavController())
     }
 }
